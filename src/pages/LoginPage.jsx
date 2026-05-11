44bca21db286a4ac2f6e6711c4e24a115a2aacbf
Mahbub049 <mahbubsarwar49@gmail.com>
2026-05-11 15:12:11 +0600

    portfolio updated

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -211,15 +211,15 @@ function LoginPage() {
                   </div>
                 </form>
 
-                <p className="mt-6 text-center text-[11px] text-slate-400 dark:text-slate-500">
-                  Portfolio Website:{" "}
+                <p className="mt-6 text-center text-[14px] text-slate-400 dark:text-slate-500">
+                  Developed By:{" "}
                   <a
                     href="https://mahbub-sarwar.vercel.app/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="font-semibold text-indigo-600 underline-offset-4 transition hover:text-indigo-700 hover:underline dark:text-indigo-400 dark:hover:text-indigo-300"
                   >
-                    mahbub-sarwar.vercel.app
+                    Mahbub Sarwar (Lecture in CSE, BUBT)
                   </a>
                 </p>
               </div>
